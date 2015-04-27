338a6e112c422f2e97ddd0f61b9fdc8ddf4f4743
jaraki <arakij@uci.edu>
2015-04-27 15:41:56 -0700

    ICS 62 Game
    Starting point for the project. Added a basic menu for navigation
    between scenes.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public void LoadLevel(string action) {
+        if (action == "Main") {
+            Application.LoadLevel("Main");
+        } else if (action == "Instructions") {
+            Application.LoadLevel("Instructions");
+        } else if (action == "Menu"){
+            Application.LoadLevel("Menu");   
+        }else if (action == "Exit") {
+            Application.Quit();
+        }
+        //Debug.Log("press");
+
+    }
+
+}
