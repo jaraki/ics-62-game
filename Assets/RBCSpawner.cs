2a8e6ae4985dfb99f5dcb9b04dcddb6c79b1459c
Dingzhu <dingzhu3@gmail.com>
2015-05-20 17:03:43 -0700

    Added Red Blood Cells
    Added Red Blood Cells, which spawn from a wall and head to another.

---
+++ Assets/RBCSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RBCSpawner : MonoBehaviour {
+
+		public Rigidbody rbc;
+		private float spawnTime;
+		// Use this for initialization
+		void Start() {
+			spawnTime = 0f;
+		}
+		
+		// Update is called once per frame
+		void Update() {
+			int randX = Random.Range(-483, 483);
+			int randY = Random.Range(0, 100);
+			Vector3 v = new Vector3(randX, randY, -495);
+			spawnTime -= Time.deltaTime;
+			if (spawnTime < 0f) {
+				Instantiate(rbc, v, new Quaternion());
+				spawnTime = 5f;
+			}
+		}
+	}
---
+++ Assets/Scripts/RedBloodCell.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedBloodCell : MonoBehaviour {
+	public int speed;
+	private int x;
+	private int y;
+	private int z;
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		x = (int)(Random.value * 180);
+		y = (int)(Random.value * 180);
+		z = (int)(Random.value * 180);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (x * Time.deltaTime, y * Time.deltaTime, z * Time.deltaTime);
+		transform.position += Vector3.forward * speed * Time.deltaTime;
+	}
+}
